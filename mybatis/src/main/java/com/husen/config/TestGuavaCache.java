cba9066b2996ca8428efd5803637981a9953ac0e
sen.hu <1178515826@qq.com>
2018-09-17 18:19:58 +0800

    重构

---
+++ mybatis/src/main/java/com/husen/config/TestGuavaCache.java
@@ -0,0 +1,7 @@
+package com.husen.config;
+
+/**
+ * Created by HuSen on 2018/9/17 13:31.
+ */
+public class TestGuavaCache {
+}
---
+++ mybatis/src/main/java/com/husen/config/TestMemcached.java
@@ -0,0 +1,7 @@
+package com.husen.config;
+
+/**
+ * Created by HuSen on 2018/9/17 16:45.
+ */
+public class TestMemcached {
+}
