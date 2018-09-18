e9ba1654ab2e077d24c20ffb557e64d9ad1940c1
sen.hu <1178515826@qq.com>
2018-09-18 11:22:43 +0800

    da

---
+++ jian/src/main/java/com/husen/jian/JianApplication.java
@@ -0,0 +1,12 @@
+package com.husen.jian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JianApplication.class, args);
+    }
+}
